f676da620903c7a084c664e4c971063f25545c96
Zsolt Döme <zsoltdome0005@gmail.com>
2023-12-17 19:34:06 +0100

    sqlite

--- config/database.php
+++ config/database.php
@@ -6,6 +6,14 @@ return [
     'default' => env('DB_CONNECTION', 'mysql'),
 
     'connections' => [
+        'sqlite' => [
+            'driver' => 'sqlite',
+            'url' => env('DATABASE_URL'),
+            'database' => env('DB_DATABASE', database_path('database.sqlite')),
+            'prefix' => '',
+            'foreign_key_constraints' => env('DB_FOREIGN_KEYS', true),
+        ],
+
         'mysql' => [
             'driver' => 'mysql',
             'url' => env('DATABASE_URL'),
